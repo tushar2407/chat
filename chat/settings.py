d4d8fbede2ed5854d9af60c5e1a26a48206a6614
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-04-29 03:47:26 +0530

    tests

--- chat/settings.py
+++ chat/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
